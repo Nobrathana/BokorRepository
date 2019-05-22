2d103abd2f422085a1d10433ca10638e363f136b
Rathana <rathanastore@gmail.com>
2019-05-22 23:26:20 +0700

    add Home Class

--- ConsoleApplication1/ConsoleApplication1/Home.cs
+++ ConsoleApplication1/ConsoleApplication1/Home.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Home
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
